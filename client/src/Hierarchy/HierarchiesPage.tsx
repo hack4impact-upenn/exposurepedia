5550039bc162db71d1609fae27946d28ad4c815a
ziyaxu <ziyaxu07@gmail.com>
2022-10-01 11:02:54 -0400

    created pages for tasks this week

---
+++ client/src/Hierarchy/HierarchiesPage.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+/**
+ * A page only accessible to authenticated users that displays hierarchies
+ * in a table and allows users to expand and delete hierarchies.
+ */
+function HierarchiesPage() {
+  return <div>View hierarchies here!</div>;
+}
+
+export default HierarchiesPage;
---
+++ client/src/Hierarchy/ViewHierarchyPage.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+/**
+ * A page only accessible to authenticated users that displays all the information
+ * about a hierarchy.
+ */
+function ViewHierarchyPage() {
+  return <div>View a specific hierarchy here!</div>;
+}
+
+export default ViewHierarchyPage;
