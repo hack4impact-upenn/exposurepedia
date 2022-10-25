fda4b84f63be336edd94378c5db5c244bc974e46
Ria Sharma <riassharma10@gmail.com>
2022-10-24 22:45:30 -0400

    creates the exposure item table component and puts it on a preliminary exposurepedia page

---
+++ client/src/Hierarchy/Exposurepedia.tsx
@@ -0,0 +1,49 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+import { ExposureItemTable } from '../components/ExposureItemTable';
+
+/**
+ * A page only accessible to authenticated users that displays hierarchies
+ * in a table and allows users to expand and delete hierarchies.
+ */
+
+function Exposurepedia() {
+  const rows = [
+    {
+      key: '1',
+      title: 'Write your own obituary',
+      format: 'Idea',
+      likes: 45,
+      date: '2021-10-10',
+    },
+    {
+      key: '2',
+      title: 'Young boy gets blood drawn',
+      format: 'Reading',
+      likes: 2,
+      date: '2022-10-2',
+    },
+    {
+      key: '3',
+      title: 'Young girl gets a painfree shot',
+      format: 'Video',
+      likes: 100,
+      date: '2021-11-04',
+    },
+  ];
+
+  const columns = [
+    { id: 'checkbox', label: '', minWidth: 15 },
+    { id: 'title', label: 'Title', minWidth: 170 },
+    { id: 'format', label: 'Format', minWidth: 100 },
+    { id: 'likes', label: 'Likes', minWidth: 100 },
+    { id: 'date', label: 'Date', minWidth: 100 },
+  ];
+  return (
+    <div>
+      <ExposureItemTable rows={rows} columns={columns} />
+    </div>
+  );
+}
+
+export default Exposurepedia;
