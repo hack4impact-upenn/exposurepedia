9e5c9a805ef1570f29bbadf0263afb3f36f18561
Joseph Zhang <josephzhang999@gmail.com>
2022-11-15 18:33:24 -0500

    Implemented routes

--- server/src/controllers/exposure.controller.ts
+++ server/src/controllers/exposure.controller.ts
@@ -3,49 +3,67 @@
  * exposure items.
  */
 import express from 'express';
-// import ApiError from '../util/apiError';
+import ApiError from '../util/apiError';
 import StatusCode from '../util/statusCode';
-import { getByID, deleteByID } from '../services/exposure.service';
+import {
+  getExposureItemFromDB,
+  updateExposureItemInDB,
+  deleteExposureItemFromDB,
+} from '../services/exposure.service';
 
 /**
- *
+ * Gets exposure item by id. Upon success, returns the item with 200 OK status code.
  */
 const getExposureItemByID = async (
   req: express.Request,
   res: express.Response,
-  // next: express.NextFunction,
+  next: express.NextFunction,
 ) => {
-  const { id } = req.params;
-  const item = await getByID(id);
-  console.log(id);
-  res.sendStatus(StatusCode.OK).send(item);
+  const id = req.params.exposure_id;
+  getExposureItemFromDB(id)
+    .then((item) => {
+      res.sendStatus(StatusCode.OK).send(item);
+    })
+    .catch(() => {
+      next(ApiError.internal('Unable to retrieve exposure item'));
+    });
 };
 
 /**
- *
+ * Updates exposure item by id. Upon success, returns 200 OK status code.
  */
 const patchExposureItemByID = async (
   req: express.Request,
   res: express.Response,
-  // next: express.NextFunction,
+  next: express.NextFunction,
 ) => {
-  const { id } = req.params;
-  console.log(id);
-  res.sendStatus(StatusCode.OK);
+  const id = req.params.exposure_id;
+  const updatedItem = req.body;
+  updateExposureItemInDB(id, updatedItem)
+    .then(() => {
+      res.sendStatus(StatusCode.OK);
+    })
+    .catch(() => {
+      next(ApiError.internal('Unable to update exposure item'));
+    });
 };
 
 /**
- *
+ * Deletes exposure item by id. Upon success, returns 200 OK status code.
  */
 const deleteExposureItemByID = async (
   req: express.Request,
   res: express.Response,
-  // next: express.NextFunction,
+  next: express.NextFunction,
 ) => {
-  const { id } = req.params;
-  const item = await deleteByID(id);
-  console.log(id);
-  res.sendStatus(StatusCode.OK).send(item);
+  const id = req.params.exposure_id;
+  deleteExposureItemFromDB(id)
+    .then(() => {
+      res.sendStatus(StatusCode.OK);
+    })
+    .catch(() => {
+      next(ApiError.internal('Unable to delete exposure item'));
+    });
 };
 
 export { getExposureItemByID, patchExposureItemByID, deleteExposureItemByID };
--- server/src/services/exposure.service.ts
+++ server/src/services/exposure.service.ts
@@ -1,16 +1,47 @@
 /**
  * All the functions for interacting with exposure data in the MongoDB database
  */
-import { ExposureItem } from '../models/exposureItem.model';
+import { ExposureItem, IExposureItem } from '../models/exposureItem.model';
 
-const getByID = async (id: string) => {
+/**
+ * Get exposure item from DB given id string.
+ * @param id The id of the exposure item
+ * @returns The item in the DB with the specified id
+ */
+const getExposureItemFromDB = async (id: string) => {
   const item = await ExposureItem.findById(id).exec();
   return item;
 };
 
-const deleteByID = async (id: string) => {
+/**
+ * Update exposure item in DB given id and new exposure item
+ * @param id The id of the exposure item to update
+ * @param updatedItem The new exposure item
+ * @returns The old exposure item
+ */
+const updateExposureItemInDB = async (
+  id: string,
+  updatedItem: IExposureItem,
+) => {
+  const item = await ExposureItem.findOneAndReplace(
+    { _id: id },
+    updatedItem,
+  ).exec();
+  return item;
+};
+
+/**
+ * Deletes the exposure item from the DB with the specified id
+ * @param id The id of the exposure item to delete
+ * @returns The deleted exposure item
+ */
+const deleteExposureItemFromDB = async (id: string) => {
   const item = await ExposureItem.findByIdAndDelete(id).exec();
   return item;
 };
 
-export { getByID, deleteByID };
+export {
+  getExposureItemFromDB,
+  updateExposureItemInDB,
+  deleteExposureItemFromDB,
+};
