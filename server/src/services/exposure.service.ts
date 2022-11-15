0b0806e56757c483ca137fddb26441976f24aae5
Ethan Weisberg <eiweisberg.2718@gmail.com>
2022-11-15 14:01:24 -0500

    adding service functions for getting items with query and filter and getting by ID

--- server/src/services/exposure.service.ts
+++ server/src/services/exposure.service.ts
@@ -1,3 +1,26 @@
 /**
  * All the functions for interacting with exposure data in the MongoDB database
  */
+import { ExposureItem } from '../models/exposureItem.model';
+
+interface Filter {
+  disorders?: string[];
+  format?: string[];
+  intervention_types?: string[];
+  keywords?: string[];
+}
+
+const getItems = async (query?: string, filters?: Filter) => {
+  const items = await ExposureItem.find({
+    name: { $search: query },
+    filters,
+  }).exec();
+  return items;
+};
+
+const getItemByID = async (id: number) => {
+  const item = await ExposureItem.findById(id).exec();
+  return item;
+};
+
+export { getItems, getItemByID };
