6b4d35b9572b848de9662f281dcf51fe979f18d1
Vincent Tiu <46623413+vincetiu8@users.noreply.github.com>
2022-11-20 17:15:54 -0500

    add hierarchy routes
    This commit adds the hierarchy routes in so we can interact with hierarchies from the front-end.

---
+++ server/src/routes/hierarchy.route.ts
@@ -0,0 +1,52 @@
+/**
+ * Specifies the middleware and controller functions to call for each route
+ * relating to admin users.
+ */
+import express from 'express';
+import 'dotenv/config';
+import {
+  createHierarchyHandler,
+  getUserHierarchies,
+  updateHierarchyHandler,
+  deleteHierarchyHandler,
+  getHierarchy,
+} from '../controllers/hierarchy.controller';
+
+const router = express.Router();
+
+/**
+ * A GET route to get a list of hierarchies created by a user.
+ * Expects the following fields in the URL:
+ * user_id (number) - The id of the user
+ */
+router.get('/:user_id', getUserHierarchies);
+
+/**
+ * A GET route to populate the View Hierarchy page for the hierarchy with the specified hierarchy_id
+ * Expects user_id and hierarchy_id
+ * Returns a hierarchy with fields: id, user_id, title, exposure_ids, updated_at
+ */
+router.get('/:user_id/:hierarchy_id', getHierarchy);
+
+/**
+ * A POST route to create a hierarchy with the fields specified from the request body.
+ * Expects user_id, title, description
+ * Returns success/failure message
+ */
+router.post('/', createHierarchyHandler);
+
+/**
+ * A PATCH route to edit a hierarchy
+ * Expects user_id, hierarchy_id (query params) and user_id, title, description, exposure_ids (body params)
+ * Returns success/failure message
+ */
+router.patch('/:user_id/:hierarchy_id', updateHierarchyHandler);
+
+/**
+ * A DELETE route to delete a hierarchy and all rows in in_hierarchy table that have the same hierarchy_id
+ * Expects hierarchy_id
+ * Returns success/failure message
+ */
+router.delete('/:hierarchy_id', deleteHierarchyHandler);
+
+export default router;
