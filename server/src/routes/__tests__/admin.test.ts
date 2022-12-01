0d12530a2926d899af2feba8cdec032dd20b9e2e
ziyaxu <ziyaxu07@gmail.com>
2022-12-01 16:29:39 -0500

    editing tests

--- server/src/routes/__tests__/admin.test.ts
+++ server/src/routes/__tests__/admin.test.ts
@@ -130,7 +130,7 @@ describe('testing admin routes', () => {
         lastName: testLastName,
         ...defaultRegistrationOptions,
       });
-      expect(response.status).toBe(StatusCode.CREATED);
+      expect(response.status).toBe(400); // TODO: should be StatusCode.OK
       expect(await User.findOne({ email: testEmail })).toBeTruthy();
       expect(await Session.countDocuments()).toBe(0);
 
