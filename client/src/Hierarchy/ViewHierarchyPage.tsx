caed0649e2ec3d7ec153c128f2d0afbfd4c452f7
helena zhou <66381768+zhouhelena@users.noreply.github.com>
2022-12-03 17:48:07 -0500

    view hierarchies frontend

--- client/src/Hierarchy/ViewHierarchyPage.tsx
+++ client/src/Hierarchy/ViewHierarchyPage.tsx
@@ -7,7 +7,9 @@
 import React from 'react';
 import ArrowBackRoundedIcon from '@mui/icons-material/ArrowBackRounded';
 import ModeRoundedIcon from '@mui/icons-material/ModeRounded';
+import DownloadRoundedIcon from '@mui/icons-material/DownloadRounded';
 import { Mode } from '@mui/icons-material';
+import { Button, TextField } from '@mui/material';
 import { ViewHierarchyTable } from '../components/ViewHierarchyTable';
 
 const ViewHierarchyPage = function () {
@@ -16,19 +18,19 @@ const ViewHierarchyPage = function () {
       key: '1',
       no: 1,
       itemName: 'Write your own obituary',
-      suds: 'cakjsna',
+      suds: '_____',
     },
     {
       key: '2',
       no: 2,
       itemName: 'Young boy gets blood drawn',
-      suds: 'casc',
+      suds: '_____',
     },
     {
       key: '3',
       no: 3,
       itemName: 'Young girl gets a painfree shot',
-      suds: 'cklsnaj',
+      suds: '_____',
     },
   ];
 
@@ -40,27 +42,95 @@ const ViewHierarchyPage = function () {
 
   return (
     <div style={{ width: '90%', margin: 'auto' }}>
-      <div style={{ display: 'flex' }}>
-        <ArrowBackRoundedIcon
-          sx={{
-            color: 'black',
-            margin: '0px 5px',
-          }}
-        />
-        <div>
-          <h1> Hierarchy 1: XYZ Disorder </h1>
-          <p style={{ padding: '0px 0px 30px 0px' }}>
-            Taran is a sophomore in college. This is a comment.
-          </p>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}
+      >
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <ArrowBackRoundedIcon
+            sx={{
+              color: 'black',
+            }}
+          />
+          <h1 style={{ padding: '0px 20px 0px 20px' }}>
+            {' '}
+            Hierarchy 1: XYZ Disorder{' '}
+          </h1>
+          <ModeRoundedIcon
+            sx={{
+              color: 'black',
+            }}
+          />
         </div>
-        <ModeRoundedIcon
-          sx={{
-            color: 'black',
-            margin: '0px 5px',
-          }}
-        />
+        <div>
+          <Button
+            variant="outlined"
+            sx={{
+              maxWidth: '180px',
+              height: '40px',
+              borderColor: 'blue',
+              color: 'blue',
+              textTransform: 'none',
+              margin: '0px 5px',
+              borderRadius: '10px',
+              background: 'rgba(255,255,255,0.8)',
+            }}
+          >
+            Export as CSV <DownloadRoundedIcon />
+          </Button>
+          <Button
+            variant="outlined"
+            sx={{
+              maxWidth: '180px',
+              height: '40px',
+              borderColor: 'red',
+              color: 'red',
+              textTransform: 'none',
+              margin: '0px 5px',
+              borderRadius: '10px',
+              background: 'rgba(255,255,255,0.8)',
+            }}
+          >
+            Delete
+          </Button>
+        </div>
       </div>
+      <p style={{ padding: '0px 0px 30px 45px' }}>
+        Taran is a sophomore in college. This is a comment.
+      </p>
       <ViewHierarchyTable rows={rows} columns={columns} />
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          paddingTop: '55px',
+        }}
+      >
+        <TextField
+          id="outlined-basic"
+          label="Add Custom Item"
+          variant="outlined"
+        />
+        <Button
+          variant="outlined"
+          sx={{
+            maxWidth: '180px',
+            height: '40px',
+            borderColor: 'blue',
+            color: 'blue',
+            textTransform: 'none',
+            margin: '0px 5px',
+            borderRadius: '10px',
+            background: 'rgba(255,255,255,0.8)',
+          }}
+        >
+          Add
+        </Button>
+      </div>
     </div>
   );
 };
